792d36437c70b986c0e7e5eea445df243ae7eb90
Jesus Gomez <gomezja@Jesuss-MacBook-Pro.local>
2017-06-21 01:27:53 -0700

    scroll feature added

--- PageScroll/ViewController.swift
+++ PageScroll/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var contentWidth: CGFloat = 0.0
+        
         // creating the images and sticking them into the array
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
@@ -27,12 +29,15 @@ class ViewController: UIViewController {
             
             newX = view.frame.midX + view.frame.size.width * CGFloat(x)
             
+            contentWidth += newX
+            
             scrollView.addSubview(imageView)
             
             imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
         }
         
-        print("Count: \(images.count)")
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        
     }
 
     override func didReceiveMemoryWarning() {
