332e79ba9ab0115dad2cfade9f3791a40e648fed
Jesus Gomez <gomezja@Jesuss-MacBook-Pro.local>
2017-06-21 11:58:52 -0700

    icon edge is now visible

--- PageScroll/ViewController.swift
+++ PageScroll/ViewController.swift
@@ -16,8 +16,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
         
         var contentWidth: CGFloat = 0.0
+        
+        let scrollWidth = scrollView.frame.size.width
         
         // creating the images and sticking them into the array
         for x in 0...2 {
@@ -27,24 +37,19 @@ class ViewController: UIViewController {
             
             var newX: CGFloat = 0.0
             
-            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
             
             contentWidth += newX
             
             scrollView.addSubview(imageView)
             
-            imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
         }
         
-        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        scrollView.clipsToBounds = false
         
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
